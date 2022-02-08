424b05a120c7b6b5de77406dc938c87821db412e
Victor <vvm0001@alu.medac.es>
2022-02-08 20:36:09 +0100

    segundo commit bot1

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ switch($message) {
         sendMessage($chatId, $response);
         break;
 }
-
+echo 'Hola';
 function sendMessage($chatId, $response) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
