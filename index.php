5deb8a9e7956e19ec2719592305f64094fb26c91
Victor <vvm0001@alu.medac.es>
2022-02-08 20:59:58 +0100

    segundo commit bot4

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $update = json_decode($input, TRUE);
 
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
-
-switch($message) {
+if(empty($message[0])){
+switch($message[0]) {
     case '/start':
         $response = 'Me has iniciado';
         sendMessage($chatId, $response);
@@ -22,9 +22,11 @@ switch($message) {
         sendMessage($chatId, $response);
         break;
 }
+}
 echo 'Hola';
 function sendMessage($chatId, $response) {
     $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
 }
+
 ?>
