41d82f7a8a941590b5c2cb36eddb0c256867d191
Victor <vvm0001@alu.medac.es>
2022-02-10 17:30:13 +0100

    segundo commit bot28

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-
 $token = '5208043466:AAFrmtgzTU5bdMkq3Br5zRyjcKp9z_VYo10';
 $website = 'https://api.telegram.org/bot'.$token;
-
+ 
 $input = file_get_contents('php://input');
 $update = json_decode($input, TRUE);
-
+ 
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
 switch($message) {
@@ -13,60 +12,57 @@ switch($message) {
         $response = 'Me has iniciado';
         sendMessage($chatId, $response);
         break;
-    case '/info':
+    case 'Hola':
         $response = 'Hola! Soy @Victorvm_bot';
         sendMessage($chatId, $response);
         break;
-    case 'Hola':
+        case 'Hola':
             $response = 'Hola! Soy el bot de Victor😉';
             sendMessage($chatId, $response);
             break;
-    case '/hora':
-               $response = 'La hora actual es ' . date('H:i:s');
-            break;
-            case '/noticias':
-                getNoticias($chatId);
-                break;
 
-        
-        }
-         default:
+        case '/ayuda2':
+            $response = "Tranquilo, estoy contigo.";
+            $keyboard = '["Gracias"],["Pos Ok"]';
+            sendMessage($chatId, $response,$keyboard);
+            break;
+        case '/noticias':
+            getNoticias($chatId);
+            break;
+    default:
         $response = 'No te he entendido';
         sendMessage($chatId, $response);
         break;
-
+ 
     }
-function sendMessage($chatId, $response) {
+function sendMessage($chatId, $response, $keyboard = NULL) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
+    if (isset($keyboard)) {
+        $teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
     }
-    
-    function getNoticias($chatId){
+}
  
-        //include("simple_html_dom.php");
-     
-        $context = stream_context_create(array('https' =>  array('header' => 'Accept: application/xml')));
-        $url = "https://www.sport.es/es/rss/tenis/rss.xml";
-        $xmlstring = file_get_contents($url, false, $context);
-     
-        $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-        $json = json_encode($xml);
-        $array = json_decode($json, TRUE);
-        
-        $titulos = $titulos."\n\n".$array['channel']['title']."<a href='".$array['channel']['item']['6']['link']."'> +info</a>";
-        
-     
-        sendMessage($chatId, $titulos);
-     
-     
-     
-    }
-     
-     
-    ?>
-     
-     
+function getNoticias($chatId){
+ 
+    //include("simple_html_dom.php");
+ 
+    $context = stream_context_create(array('https' =>  array('header' => 'Accept: application/xml')));
+    $url = "https://e00-marca.uecdn.es/rss/futbol/barcelona.xml";
+    $xmlstring = file_get_contents($url, false, $context);
+ 
+    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json = json_encode($xml);
+    $array = json_decode($json, TRUE);
     
-
-
-
+    $titulos = $titulos."\n\n".$array['channel']['title']."<a href='".$array['channel']['item']['6']['link']."'> +info</a>";
+    
+ 
+    sendMessage($chatId, $titulos);
+ 
+ 
+ 
+}
+ 
+ 
+?>
