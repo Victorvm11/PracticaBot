65fbd8c9dcecb038eb0d357034c148a8f67905cb
Victor <vvm0001@alu.medac.es>
2022-02-10 17:25:28 +0100

    segundo commit bot26

--- index.php
+++ index.php
@@ -49,12 +49,7 @@ switch($message) {
             case '/noticias':
                 getNoticias($chatId);
                 break;
-            case '/participar': case '/participar@VazCell_bot':
-                getSorteos($chatId, $message, $userId, $firstname, $agg);
-                break;
-            case '/youtube':
-                sendMessage($chatId, "Mi canal de YouTube es <a href='https://www.youtube.com/channel/UCGArCE3vmQkFpu_o_6axt1g'>SrVazquez</a>");
-            break;
+
         
         }
          default:
@@ -66,12 +61,6 @@ switch($message) {
 function sendMessage($chatId, $response) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
-    function sendMessage($chatId, $response, $keyboard = NULL){
-        if (isset($keyboard)) {
-            $teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
-        }
-        $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response).$teclado;
-        file_get_contents($url);
     }
     
     function getNoticias($chatId){
@@ -87,9 +76,8 @@ function sendMessage($chatId, $response) {
         $json = json_encode($xml);
         $array = json_decode($json, TRUE);
     
-        for ($i=0; $i < 9; $i++) { 
-            $titulos = $titulos."\n\n".$array['channel']['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-        }
+            $titulos = $titulos."\n\n".$array['channel']['title']."<a href='".$array['channel']['item']['1']['link']."'> +info</a>";
+        
     
         sendMessage($chatId, $titulos);
     
