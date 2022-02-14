d889a96ed311997750075fc66dd5feb54c805276
Victor <vvm0001@alu.medac.es>
2022-02-14 15:58:01 +0100

    segundo commit bot34

--- index.php
+++ index.php
@@ -3,65 +3,100 @@ $token = '5208043466:AAFrmtgzTU5bdMkq3Br5zRyjcKp9z_VYo10';
 $website = 'https://api.telegram.org/bot'.$token;
  
 $input = file_get_contents('php://input');
-$update = json_decode($input, TRUE);
- 
-$chatId = $update['message']['chat']['id'];
-$message = $update['message']['text'];
-switch($message) {
-    case '/start':
-        $response = 'Me has iniciado';
-        sendMessage($chatId, $response);
-        break;
-    case '/info':
-        $response = 'Hola! Soy @Victorvm_bot';
-        sendMessage($chatId, $response);
-        break;
-        case 'Hola':
-            $response = 'Hola! Soy el bot de Victor😉';
-            sendMessage($chatId, $response);
-            break;
+$update = json_decode($data,true);
+$message = $update['message'];
 
-        case '/ayuda2':
-            $response = "Tranquilo, estoy contigo.";
-            $keyboard = '["Gracias"],["Pos Ok"]';
-            sendMessage($chatId, $response,$keyboard);
-            break;
-        case '/noticias':
-            getNoticias($chatId);
-            break;
-    default:
-        $response = 'No te he entendido';
-        sendMessage($chatId, $response);
-        break;
- 
-    }
-function sendMessage($chatId, $response, $keyboard = NULL) {
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
-    file_get_contents($url);
-    if (isset($keyboard)) {
-        $teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
-    }
+$id = $message["from"]["id"];
+$name = $message["from"]["first_name"];
+$text = $message["text"];
+
+
+/*
+if(isset($text) &&  $text =='/start')
+{
+	$respuesta = "Hola ".$name." -- Bienvenido 😜 a mi Último BOT";
+	sendMessage($id,$respuesta,$token);
 }
- 
-function getNoticias($chatId){
- 
-    //include("simple_html_dom.php");
- 
-    $context = stream_context_create(array('https' =>  array('header' => 'Accept: application/xml')));
-    $url = "https://www.sport.es/es/rss/tenis/rss.xml";
-    $xmlstring = file_get_contents($url, false, $context);
- 
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    
-    $titulos = $titulos."\n\n".$array['channel']['title']."<a href='".$array['channel']['item']['1']['link']."'> +info</a>";
-    
- 
-    sendMessage($chatId, $titulos);
- 
- 
- 
+
+else if(isset($text) &&  $text =='/help')
+{
+
+	$respuesta = "Este es un Robot de Prueba para Telegram Hecho 100% en PHP, sin librerias Externas en un solo archivo\n\n".
+	'Y funciona en cualquier hosting  asi sea barato, solo debe tener  HTTPS.';
+	sendMessage($id,$respuesta,$token);
+}
+
+else if(isset($text) &&  $text =='uptime')
+{
+	$res = shell_exec("uptime");
+	$respuesta = $res;
+
+	sendMessage($id,$respuesta,$token);
+}
+*/
+
+if(isset($text) &&  $text =='❌ CANCELAR')
+{
+
+$keyboard= [
+	['Opción 1','Opción 2'],
+	['❌ CANCELAR']
+];
+
+	$key = array('one_time_keyboard' => true,'resize_keyboard' => true,'keyboard' => $keyboard);
+	$k=json_encode($key);
+
+
+	$respuesta = "USTED HA CANCELADO";
+	sendMessage($id,$respuesta,$token);
+
+
+	die();
+}
+
+
+
+
+
+$keyboard= [
+	['📌100','200','300'],
+	['400','500'],
+	['❌ CANCELAR'],
+];
+
+$key = array('one_time_keyboard' => true,'resize_keyboard' => true,'keyboard' => $keyboard);
+$k=json_encode($key);
+
+
+	$respuesta =' Hola';
+    sendMessage($id,$respuesta,$token,$k);
+
+
+
+
+
+
+
+
+
+
+
+function sendMessage($chatID, $messaggio, $token,&$k = ''){
+    $url = "https://api.telegram.org/" . $token . "/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=" . $chatID;
+
+	if(isset($k)) {
+		$url = $url."&reply_markup=".$k; 
+		}
+
+    $url = $url."&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
 }
  
  
