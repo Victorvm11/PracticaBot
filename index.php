198fc3bbedc4a329fe46d09167c02b8a025f3424
Victor <vvm0001@alu.medac.es>
2022-02-18 17:56:24 +0100

    segundo commit bot69

--- index.php
+++ index.php
@@ -97,6 +97,15 @@ switch($message) {
     
 
 */
+$keyboard = array('keyboard' => 
+            array(array( 
+                array('text'=>'/juegos','callback_data'=>"1"), 
+            ), 
+                array( 
+                    array('text'=>'/easter egg','callback_data'=>"4") 
+                )), 'one_time_keyboard' => false, 'resize_keyboard' => true 
+        ); 
+        file_get_contents('https://api.telegram.org/bot5208043466:AAFrmtgzTU5bdMkq3Br5zRyjcKp9z_VYo10/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($keyboard).'&text=Elija que desea hacer');
 
 function sendMessage($chatId, $response, $repl) {
     if($repl==TRUE){
