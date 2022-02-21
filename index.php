1282b8aaa5ff2c7df75817a4870bdb1ccef5a619
Victor <vvm0001@alu.medac.es>
2022-02-21 15:13:46 +0100

    segundo commit bot84

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ switch($message) {
                 case '/teclado':
                     $keyboard = array('keyboard' => 
             array(array( 
-                array('text'=>'/hora','callback_data'=>"1"), 
+                array('text'=>'/hora','callback_data'=>"5"), 
             ), 
                 array( 
                     array('text'=>'/fecha','callback_data'=>"4") 
