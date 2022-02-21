9f342c04bb6d3befae73ca585afd14878ce07bfc
Victor <vvm0001@alu.medac.es>
2022-02-21 15:33:03 +0100

    segundo commit bot87

--- index.php
+++ index.php
@@ -27,12 +27,12 @@ switch($message) {
             break;
             case '/fecha':
                 $response  = 'La fecha actual es  ' . date('d/m/Y');
-                sendMessage($chatId, $response);
+                sendMessage($chatId, $response,FALSE);
                 break;
        
             case '/hora':
                 $response  = 'La hora actual es ' . date('H:i:s');
-                sendMessage($chatId, $response);
+                sendMessage($chatId, $response,FALSE);
             break;
             case '/help':
                 $response  = 'Los comandos disponibles son:
